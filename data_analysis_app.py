c29d1b3366e71900c9662ecc8ce5c80ae76a5c85
Juan Carlos OMB <petonortiz@gmail.com>
2023-11-09 21:23:49 -0600

    Update data_analysis_app.py
    Vamos a estructurar la función de procesamiento. Esta función se encargará de aplicar cualquier filtro especificado por el usuario, como un rango de fechas, y luego limpiar los datos (por ejemplo, llenar los valores vacíos).

--- data_analysis_app.py
+++ data_analysis_app.py
@@ -1,30 +1,60 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import pandas as pd
+import sqlite3
 
-# Esta función se llamará cuando el usuario haga clic en el botón "Cargar CSV"
+# Variables globales
+df = None  # DataFrame para almacenar los datos cargados
+
+# Función para cargar CSV
 def cargar_csv():
-    # Abrir el diálogo de selección de archivos y obtener la ruta del archivo
-    filepath = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv"), ("All files", "*.*")])
+    global df
+    filepath = filedialog.askopenfilename(
+        filetypes=[("CSV files", "*.csv"), ("All files", "*.*")]
+    )
     if not filepath:  # Si no se selecciona ningún archivo, no hacer nada
         return
     try:
-        # Leer el archivo CSV con pandas y posiblemente almacenarlo en alguna estructura
         df = pd.read_csv(filepath)
-        # Aquí puedes añadir el dataframe a una lista o directamente a una base de datos
-        print(df)  # Solo para fines de depuración, mostrar el dataframe en la consola
+        messagebox.showinfo("Cargar CSV", "Archivo cargado con éxito")
     except Exception as e:
-        tk.messagebox.showerror("Error al leer el archivo", str(e))
-        return
+        messagebox.showerror("Error al leer el archivo", str(e))
 
-# Configuración de la ventana principal y el botón de carga
+# Función para procesar datos
+def procesar_datos():
+    global df
+    if df is None:
+        messagebox.showerror("Procesar Datos", "No hay datos para procesar. Cargue un archivo CSV primero.")
+        return
+    try:
+        # Aquí irían los filtros y el procesamiento de datos
+        # Por ejemplo, reemplazar NaN por 0 (esto es solo un ejemplo, modifícalo según tus necesidades)
+        df.fillna(0, inplace=True)
+        
+        # Almacenar los datos en SQLite
+        with sqlite3.connect('datos_temporales.db') as conn:
+            df.to_sql('datos_procesados', conn, if_exists='replace', index=False)
+        
+        messagebox.showinfo("Procesar Datos", "Los datos han sido procesados y almacenados en la base de datos.")
+    except Exception as e:
+        messagebox.showerror("Error en el procesamiento de datos", str(e))
+
+# Configuración de la ventana principal
 root = tk.Tk()
 root.title("Aplicación de Análisis de Datos")
 
+# Frame para la carga de CSV
 frame_carga_csv = tk.Frame(root)
 frame_carga_csv.pack(padx=10, pady=10)
 
 btn_cargar_csv = tk.Button(frame_carga_csv, text="Cargar CSV", command=cargar_csv)
-btn_cargar_csv.pack()
+btn_cargar_csv.pack(side=tk.LEFT, padx=5, pady=5)
+
+# Frame para el procesamiento de datos
+frame_procesamiento = tk.Frame(root)
+frame_procesamiento.pack(padx=10, pady=10)
+
+btn_procesar_datos = tk.Button(frame_procesamiento, text="Procesar Datos", command=procesar_datos)
+btn_procesar_datos.pack(side=tk.LEFT, padx=5, pady=5)
 
 root.mainloop()
