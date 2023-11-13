d89264c424fdac98ef598bfa999c6b42b8ed0b36
Juan Carlos OMB <56810855+JC-OMB@users.noreply.github.com>
2023-11-13 12:23:00 -0600

    Exportacion de Datos 1.0
    Agregue la exportacion de datos en csv y de metadatos en json, hace falta agregar las operaciones que se deben realizar a la hora de dar click en "procesar datos" y comprobar que en efecto se estandarizen, asi como la seleccion de los datos que se van a usar, ya que aqui se deben agregar a mano ejemplo "surfTempWater"

--- data_analysis_app.py
+++ data_analysis_app.py
@@ -4,9 +4,7 @@ import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-# Variables globales
-df = None  # DataFrame para almacenar los datos cargados
+import json
 
 # Función para cargar CSV
 def cargar_csv():
@@ -14,7 +12,7 @@ def cargar_csv():
     filepath = filedialog.askopenfilename(
         filetypes=[("CSV files", "*.csv"), ("All files", "*.*")]
     )
-    if not filepath:  # Si no se selecciona ningún archivo, no hacer nada
+    if not filepath:
         return
     try:
         df = pd.read_csv(filepath)
@@ -29,17 +27,73 @@ def procesar_datos():
         messagebox.showerror("Procesar Datos", "No hay datos para procesar. Cargue un archivo CSV primero.")
         return
     try:
-        # Aquí irían los filtros y el procesamiento de datos
-        # Por ejemplo, reemplazar NaN por 0 (esto es solo un ejemplo, modifícalo según tus necesidades)
         df.fillna(0, inplace=True)
-        
-        # Almacenar los datos en SQLite
         with sqlite3.connect('datos_temporales.db') as conn:
             df.to_sql('datos_procesados', conn, if_exists='replace', index=False)
-        
-        messagebox.showinfo("Procesar Datos", "Los datos han sido procesados y almacenados en la base de datos.")
+        messagebox.showinfo("Procesar Datos", "Los datos han sido procesados y almacenados.")
     except Exception as e:
         messagebox.showerror("Error en el procesamiento de datos", str(e))
+
+# Función para visualizar datos
+def visualizar_datos():
+    try:
+        conn = sqlite3.connect('datos_temporales.db')
+        df = pd.read_sql_query("SELECT * FROM datos_procesados", conn)
+        conn.close()
+
+        if df.empty:
+            messagebox.showwarning("Visualización", "No hay datos para visualizar.")
+            return
+
+        plt.figure(figsize=(6, 4))
+        # Aquí puedes modificar el gráfico según tus necesidades, AQUI SE MODIFICAN LOS DATOS A MOSTRAR
+        
+        plt.plot(df['surfWaterTempMean'])  # Ejemplo simple
+
+        fig = plt.gcf()
+        canvas = FigureCanvasTkAgg(fig, master=root)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack()
+        canvas.draw()
+    except Exception as e:
+        messagebox.showerror("Error en la visualización", str(e))
+
+# Función para exportar datos
+def exportar_datos():
+    try:
+        conn = sqlite3.connect('datos_temporales.db')
+        df = pd.read_sql_query("SELECT * FROM datos_procesados", conn)
+        conn.close()
+
+        filepath = filedialog.asksaveasfilename(defaultextension='.csv',
+                                                filetypes=[("CSV files", "*.csv"), ("All files", "*.*")])
+        if not filepath:
+            return
+
+        df.to_csv(filepath, index=False)
+        messagebox.showinfo("Exportar Datos", "Datos exportados con éxito en formato CSV.")
+    except Exception as e:
+        messagebox.showerror("Error en la exportación de datos", str(e))
+
+# Función para exportar metadatos
+def exportar_metadatos():
+    metadatos = {
+        "fecha": "2023-11-13",
+        "usuario": "nombre_usuario",
+        "nombre_proyecto": "Proyecto X",
+        "sensores": "Sensor1, Sensor2",
+        "lugar": "Ubicación X"
+    }
+
+    filepath = filedialog.asksaveasfilename(defaultextension='.json',
+                                            filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
+    if not filepath:
+        return
+
+    with open(filepath, 'w') as f:
+        json.dump(metadatos, f)
+
+    messagebox.showinfo("Exportar Metadatos", "Metadatos exportados con éxito en formato JSON.")
 
 # Configuración de la ventana principal
 root = tk.Tk()
@@ -59,38 +113,16 @@ frame_procesamiento.pack(padx=10, pady=10)
 btn_procesar_datos = tk.Button(frame_procesamiento, text="Procesar Datos", command=procesar_datos)
 btn_procesar_datos.pack(side=tk.LEFT, padx=5, pady=5)
 
-
-def visualizar_datos():
-    try:
-        # Conectar a SQLite y obtener los datos
-        conn = sqlite3.connect('datos_temporales.db')
-        df = pd.read_sql_query("SELECT * FROM datos_procesados", conn)
-        conn.close()
-
-        # Asegurarse de que hay datos para visualizar
-        if df.empty:
-            messagebox.showwarning("Visualización", "No hay datos para visualizar.")
-            return
-
-        # Crear la figura y el canvas para Matplotlib
-        plt.figure(figsize=(6, 4))
-        # Aquí puedes añadir el código para generar el gráfico que necesites
-        plt.plot(df['surfWaterTempMean'])
-
-        # Mostrar el gráfico en Tkinter
-        fig = plt.gcf()  # Obtener la figura actual de Matplotlib
-        canvas = FigureCanvasTkAgg(fig, master=root)  # Crear un canvas de Tkinter
-        canvas_widget = canvas.get_tk_widget()
-        canvas_widget.pack()
-        canvas.draw()
-
-    except Exception as e:
-        messagebox.showerror("Error en la visualización", str(e))
-
-# ... (tu código de la GUI, como la creación de botones)
-
 # Botón para visualizar los datos
 btn_visualizar_datos = tk.Button(root, text="Visualizar Datos", command=visualizar_datos)
 btn_visualizar_datos.pack()
+
+# Botón para exportar los datos
+btn_exportar_datos = tk.Button(root, text="Exportar Datos", command=exportar_datos)
+btn_exportar_datos.pack()
+
+# Botón para exportar metadatos
+btn_exportar_metadatos = tk.Button(root, text="Exportar Metadatos", command=exportar_metadatos)
+btn_exportar_metadatos.pack()
 
 root.mainloop()
