69883f22a2a0b5e768215e27758aa0bdad3f6853
pinillation96 <44420238+pinillation96@users.noreply.github.com>
2023-11-24 13:44:38 -0600

    Update a home

--- src/frontend/components/widgets/home_frame/home_frame.py
+++ src/frontend/components/widgets/home_frame/home_frame.py
@@ -1,4 +1,4 @@
-from tkinter import Frame
+from tkinter import Frame,Label,ttk,Text
 
 
 class HomeFrame(Frame):
@@ -24,6 +24,18 @@ class HomeFrame(Frame):
     def add_widgets(self):
         # TODO - Add widgets for home frame
         # Título, fotos, enlaces a la documentación, etc.
+        text_label = ttk.Label(self, text="Nebraska Project", font=("Helvetica", 25, "bold"))
+        text_label.pack()
+        description_text = Text(self, wrap="word", height=5, width=40)
+        description_text.insert("1.0", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                                      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+                                      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "
+                                      "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
+                                      "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+                                      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
+                                      "deserunt mollit anim id est laborum.")
+        description_text.config(state="disabled")
+        description_text.pack(pady=10)
         pass
 
     def render(self):
